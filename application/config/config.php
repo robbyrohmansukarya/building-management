40f47668f18d55fa3f5932623d0443bb8d1ccee0
robbyrohmansukarya <32896386+robbyrohmansukarya@users.noreply.github.com>
2021-02-03 09:22:20 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -14,11 +14,11 @@
 | path to your installation.
 |
 */
-//$config['base_url']	= 'http://localhost/bi-baru/';
+$config['base_url']	= 'https://mis-komplain.herokuapp.com/';
 //$config['base_url']	= 'http://localhost/bi-andromeda-v2.net/';
-$config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
-$config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
-$config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
+// $config['base_url'] = ((isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] == "on") ? "https" : "http");
+// $config['base_url'] .= "://" . $_SERVER['HTTP_HOST'];
+// $config['base_url'] .= str_replace(basename($_SERVER['SCRIPT_NAME']), "", $_SERVER['SCRIPT_NAME']);
 /*
 |--------------------------------------------------------------------------
 | Index File
